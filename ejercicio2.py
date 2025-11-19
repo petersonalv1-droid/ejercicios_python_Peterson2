a4882fc6ffcb2e4f542cf3132a2171d57746f3cc
petersonalv1-droid <petersonalv1@gmail.com>
2025-11-19 21:08:41 +0000

    Calculo el factorial y lo enseño en la terminal

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,4 +3,11 @@ try:
     if n < 0:
         raise ValueError("El número no puede ser negativo.")
 
+    resultado = 1
+    while n > 1:
+        resultado *= n
+        n -= 2
+
+    print("El doble factorial es:", resultado)
+
     
